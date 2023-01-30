d3d30ca5baad40aa83cb456e692fbd6f361fea95
gkumar-pandey <gautamshekhar078@gmail.com>
2023-01-31 00:09:27 +0530

    add pattern print solution

--- JAVA/DAY-1/Basic.java
+++ JAVA/DAY-1/Basic.java
@@ -1,5 +1,14 @@
 public class Basic {
     public static void main(String args[]) {
-        System.out.print("Hello world i am learing Java");
+        System.out.print("Hello world i am learing Java"); // rrint output
+        // -> for print with new line
+        // 1. use of println() function
+        System.out.println("Hello in new line");
+        System.out.println("New line is here");
+
+        // 2. print now line wiht use of '\n'
+        System.out.print("Hello with new line\n");
+        System.out.print("Hello");
+
     }
 }
